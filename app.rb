20511c46c9781a18e5448b0b5e36d981b601283d
Karla Buckner and Rebecca McIntosh <rebecca@>
2017-09-11 11:44:48 -0700

    Add Sinatra/front end functions.

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require('sinatra')
+# require('sinatra/reloader')
+require('./lib/triangle')
+# also_reload('lib/**/*.rb')
 
-get('/triangle') do
-  "This application will determine whether a triangle is equilateral, isosceles, or scalene."
+get('/') do
+  erb(:input)
+end
+
+get('/output') do
+  # @length1 = params.fetch("length1")
+  # @length2 = params.fetch("length2")
+  # @length3 = params.fetch("length3")
+  triangle_input = Triangle.new(params.fetch("length1").to_i, params.fetch("length2").to_i, params.fetch("length3").to_i)
+  @triangle_output = triangle_input.type()
+    # @string_to_display = "This cannot be a triangle, please enter new lengths."
+    # @string_to_display = "This is an equilateral triangle."
+    # @string_to_display = "This is an isosceles triangle."
+    # @string_to_display = "This is a scalene triangle."
+    erb(:output)
 end
